71b37522889df7a3465e3dec870807357f70ba4c
geovanigcs <geovanigcs.dev@gmail.com>
2024-11-07 14:12:18 -0300

    Feat: Add LoginPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+// import { LoginPage } from "./login/page"
+
 const Home = () => {
   return (
-    <h1 className="text-red-500" >Hello Word</h1>
-  )
-}
- 
+    <>
+      <h1 className="text-red-500">Hello Word</h1>
+    </>
+  );
+};
+
 export default Home;
